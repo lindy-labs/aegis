701b5a51b369e6e6b3f7952ff959cbdcf46d04a1
Jakob von Raumer <jakob@von-raumer.de>
2023-03-24 15:09:06 +0100

    start implementing parser

--- Parser.lean
+++ Parser.lean
@@ -1,2 +1,43 @@
 import Megaparsec
 
+open Megaparsec Parsec Common
+
+inductive Identifier where
+  | atom (s : String) (is : List Identifier)
+  | ns (s : String) (i : Identifier)
+  deriving Repr
+
+-- TODO add `Nat` constants inside the pointy brackets
+
+instance : ToString Identifier where
+  toString x := toString $ repr x
+
+abbrev P := Parsec Char String Unit  -- TODO replace the `Unit` by a proper error type
+
+def atomP : P String := do
+  let c : Char ← satisfy Char.isAlpha
+  let cs : List Char ← many' (satisfy fun c => c.isAlphanum || c == '_')
+  return String.mk (c :: cs)
+
+mutual
+
+partial def atomIdentifierP : P Identifier := do
+  return .atom (← atomP) []
+
+partial def nsIdentifierP : P Identifier := do
+  let ns ← atomP
+  discard (string "::")
+  let i ← identifierP
+  return .ns ns i
+
+partial def identifierP : P Identifier :=
+  nsIdentifierP <|> atomIdentifierP
+
+end
+
+def parseGrammar (code : String) : Except String Identifier :=
+  Except.mapError toString $ parse identifierP code
+
+def code := "felt252_do::foo"
+
+#eval parseGrammar code
