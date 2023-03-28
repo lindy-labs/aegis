e140cec687c0a81436c1a35b2139231248caa91c
Jakob von Raumer <jakob@von-raumer.de>
2023-03-28 19:39:51 +0200

    find good funcdata format

--- SierraLean.lean
+++ SierraLean.lean
@@ -1,11 +1,24 @@
-import SierraLean.Parser
+--import SierraLean.Parser
 import Mathlib.Data.ZMod.Basic
 
 open Lean
 
 namespace Sierra
 
+abbrev Addr := Nat
 
+def PRIME := 3618502788666131213697322783095070105623107215331596699973092056135872020481
+
+abbrev F := ZMod PRIME
+
+-- TODO this has to exist somewhere?
+def OfInputs (R : Type) : List Type → Type
+| []        => R
+| (T :: Ts) => T → OfInputs R Ts
+
+def OfInputs.const {R : Type} (r : R) : {Ts : List Type} → OfInputs R Ts
+| []       => r
+| (_ :: _) => fun _ => OfInputs.const r
 
 abbrev RefTable := HashMap Nat FVarId
 
@@ -14,11 +27,26 @@ structure state
   (refTypes : HashMap Nat Expr)
   (conditions : List Expr)
 
-class FuncData (name : String) (refsIn : List Nat) (refsOut : List Nat) where
-  (refTableChange : RefTable → RefTable := id)
-  (condition : Expr)
-  (pcChange : Nat → Nat := (· + 1))
+class FuncData (name : String) where
+  (inputTypes : List Type := [])
+  (outputTypes : List Type := [])
+  (condition : OfInputs Prop (inputTypes ++ outputTypes) := OfInputs.const True)
+  (refsChange : OfInputs (RefTable → RefTable) (inputTypes ++ outputTypes) := OfInputs.const id)
+  (pcChange : OfInputs (Nat → Nat) (inputTypes ++ outputTypes) := OfInputs.const (· + 1))
+  
+def spec_felt252_add (a b ρ : F) : Prop := ρ = a + b
 
-def spec_felt252_add (a b ρ : F): Prop := ρ = a + b
+instance : FuncData "felt252_add" where
+  condition := fun a b ρ => ρ = a + b
+  inputTypes := [F, F]
+  outputTypes := [F]
 
+instance : FuncData "felt252_sub" where
+  condition := fun a b ρ => ρ = a - b
+  inputTypes := [F, F]
+  outputTypes := [F]
 
+instance : FuncData "rename" where
+  inputTypes := [Addr]
+  outputTypes := [Addr]
+  refsChange := fun a ρ rt => (rt.insert ρ (rt.find! a)).erase a
--- lakefile.lean
+++ lakefile.lean
@@ -9,7 +9,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "1b816f73febb09913b30c973d6fa08f5c6462eb0"
 
 require Megaparsec from git
-  "https://github.com/yatima-inc/Megaparsec.lean" @ "8859f129d199d5be82962140e9b886c0fd49e89c"
+  "https://github.com/yatima-inc/Megaparsec.lean" @ "7331b206ce132746ff7653ac63d1ff9491c8f8c9"
 
 lean_lib SierraLean {
   -- add library configuration options here
