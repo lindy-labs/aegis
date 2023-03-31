316616f458f6c5ed7a5edf1141d0a1565fed600d
Jakob von Raumer <jakob@von-raumer.de>
2023-03-31 13:22:30 +0200

    switch from State to Reader for reference table

--- SierraLean/Analyzer.lean
+++ SierraLean/Analyzer.lean
@@ -13,66 +13,72 @@ def getTypeRefs (f : SierraFile) : HashMap Identifier Identifier := HashMap.ofLi
 
 def getLibfuncRefs (f : SierraFile) : HashMap Identifier Identifier := HashMap.ofList f.libfuncs
 
-def RefTable.getOrMkNew (refs : RefTable) (n : ℕ) : MetaM (RefTable × FVarId) :=
+structure State where
+  (conditions : List Expr)
+  (pc : Nat)
+
+abbrev M := StateT State MetaM
+
+def withGetOrMkNewRef (refs : RefTable) (n : ℕ) (type : Expr) 
+    (k : RefTable → FVarId → M α) : M α :=
   match refs.find? n with
-  | .some x => return (refs, x)
+  | .some x => k refs x
   | _ => do
-    let fv ← mkFreshFVarId
-    return (refs.insert n fv, fv)
+    withLocalDeclD ("ref" ++ n.repr : String) type fun e =>
+      let fv := e.fvarId!
+      k (refs.insert n fv) fv
+
+def withGetOrMkNewRefs (refs : RefTable) (ns : List ℕ) (types : List Expr) (fvs : List FVarId := [])
+    (k : RefTable → List FVarId → M α) [Inhabited (M α) ]: M α :=
+  match ns, types with
+  | (n :: ns), (t :: ts) => withGetOrMkNewRef refs n t fun refs' fv =>
+                              withGetOrMkNewRefs refs' ns ts (fv :: fvs) k
+  | [],        []        => k refs fvs
+  | _,         _         => panic "types and ref list not the same length!"
 
 def Expr.mkAnds : List Expr → Expr
 | []        => mkConst `True
 | [e]       => e
 | (e :: es) => mkApp (mkApp (mkConst `And) e) (mkAnds es)
 
-structure State where
-  (refs : HashMap Nat FVarId)
-  (types : HashMap Nat Expr)
-  (conditions : List Expr)
-  (pc : Nat)
-
-abbrev M := StateT State MetaM
-
-def statementStep (f : SierraFile) : M Unit := do
-  let types := getTypeRefs f
+def withStatementStep (f : SierraFile) (refs : RefTable) 
+    (k : RefTable → List FVarId → M α) [Inhabited (M α)] : M α := do
   let libfuncs := getLibfuncRefs f
-  let ⟨refs, types, conditions, pc⟩ ← get
+  let ⟨conditions, pc⟩ ← get
   let ⟨i, inputs, outputs⟩ := f.statements.get! pc
   let i' := libfuncs.find! i
   match i' with 
   | (.name istr []) =>
     let fd : Expr := mkConst ("Sierra" ++ "FuncData" ++ istr)  -- TODO add parameters
     let fd_condition : Expr ← Meta.mkProjection fd `condition  -- The plain condition
-    let mut refs := refs
-    let mut input_exprs := []
-    let mut output_exprs := []
-    for n in inputs do
-      let (refs', fv) ← RefTable.getOrMkNew refs n
-      refs := refs'
-      input_exprs := input_exprs ++ [Expr.fvar fv]
-    for n in outputs do
-      let (refs', fv) ← RefTable.getOrMkNew refs n
-      refs := refs'
-      output_exprs := output_exprs ++ [Expr.fvar fv]
-    let fd_condition := mkAppN fd_condition (input_exprs ++ output_exprs).toArray
-    let conditions' := conditions ++ [fd_condition]
-    let fd : FuncData i' := FuncData_register i'
-    let pc' := fd.pcChange pc
-    set (⟨refs, types, conditions', pc'⟩ : State)
-    return
-  | _ => return
+    let fd_inputTypes : Expr ← Meta.mkProjection fd `inputTypes
+    let fd_outputTypes : Expr ← Meta.mkProjection fd `outputTypes
+    let fd_types : Expr ← mkAppM `List.append #[fd_inputTypes, fd_outputTypes]
+    let mut fd_typeList : List Expr := []
+    for i in [:(inputs.length+outputs.length)] do
+      fd_typeList := fd_typeList ++ [← mkAppM `List.get! #[fd_types, .lit <| .natVal i]]
+    withGetOrMkNewRefs refs (inputs ++ outputs) fd_typeList [] fun refs fvs => do
+      let fves := fvs.map Expr.fvar
+      let fd_condition := mkAppN fd_condition fves.toArray
+      let conditions' := conditions ++ [fd_condition]
+      let fd : FuncData i' := FuncData_register i'
+      let pc' := fd.pcChange pc
+      set (⟨conditions', pc'⟩ : State)
+      k refs fvs
+  | _ => k refs []
 
-partial def statementLoop (f : SierraFile) (finputs : List (Nat × Identifier)) : M Expr := do
-  let ⟨refs, types, conditions, pc⟩ ← get
-  let ⟨i, inputs, outputs⟩ := f.statements.get! pc
+partial def statementLoop (f : SierraFile) (finputs : List (Nat × Identifier))
+    (refs : RefTable := HashMap.empty) : M Expr := do
+  let ⟨conditions, pc⟩ ← get
+  let ⟨i, _, _⟩ := f.statements.get! pc
   match i with
   | .name "return" [] =>
     let e := Expr.mkAnds conditions
     let e ← mkForallFVars (refs.toList.map (fun (_, fv) => .fvar fv)).toArray e
     return e
   | _ =>
-    statementStep f
-    statementLoop f finputs
+    withStatementStep f refs fun refs _ =>
+      statementLoop f finputs refs
 
 def sf : SierraFile :=
 { typedefs := [(Identifier.ref 0, Identifier.name "felt252" [])],
@@ -80,9 +86,10 @@ def sf : SierraFile :=
   statements := [(Identifier.ref 0, [0, 1], [2]), (Identifier.ref 0, [1, 2], [3]), (Identifier.name "return" [], [2], [])],
   declarations := [(Identifier.ref 0, 0, [(0, Identifier.ref 0), (1, Identifier.ref 0)], [Identifier.ref 2])] }
 
-def statementStepAndReturn (f : SierraFile) : MetaM Expr := do
-  let e ← StateT.run (statementLoop f [(0, Identifier.ref 0), (1, Identifier.ref 0)]) ⟨HashMap.empty, HashMap.empty, [], 0⟩
-  return e.1
+def statementStepAndReturn (f : SierraFile) : MetaM Format := do
+  let e ← StateT.run (statementLoop f [(0, Identifier.ref 0), (1, Identifier.ref 0)]) ⟨[], 0⟩
+  ppExpr e.1
 
-#check Expr.forallE
+#check ppExpr
 #eval statementStepAndReturn sf
+
