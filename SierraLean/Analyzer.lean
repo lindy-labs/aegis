91b2419a2cee0308f1e7f7c49116c70522621901
Jakob von Raumer <jakob@von-raumer.de>
2023-04-10 21:25:52 +0200

    change state from list to tree

--- SierraLean/Analyzer.lean
+++ SierraLean/Analyzer.lean
@@ -10,8 +10,54 @@ def getTypeRefs (f : SierraFile) : HashMap Identifier Identifier := HashMap.ofLi
 
 def getLibfuncRefs (f : SierraFile) : HashMap Identifier Identifier := HashMap.ofList f.libfuncs
 
+/-- A tree to contain expressions to be composed with `And` and `Or`.
+If we want to avoid trees, this has to be replaced by some graph structure in the future. -/
+inductive AndOrTree 
+| nil  : AndOrTree
+| cons : Expr → List AndOrTree → AndOrTree
+
+-- TODO delete
+def Expr.mkAnds : List Expr → Expr
+| []        => mkConst ``True
+| [e]       => e
+| (e :: es) => mkApp (mkApp (mkConst ``And) e) (mkAnds es)
+
+/-- Build the disjunction of a list of expressions -/
+def Expr.mkOrs : List Expr → Expr
+| []        => mkConst ``False
+| [e]       => e
+| (e :: es) => mkApp (mkApp (mkConst ``Or) e) <| mkOrs es
+
+/-- Compile an `AndOrTree` down to a single expression -/
+partial def AndOrTree.toExpr : AndOrTree → Expr
+| nil       => mkConst ``True
+| cons e [] => e
+| cons e ts => mkApp (mkApp (mkConst ``And) e) <| Expr.mkOrs <| (AndOrTree.toExpr <$> ts)
+
+/-- Filter an `AndOrTree` by a boolean predicate on expressions -/
+partial def AndOrTree.filter (p : Expr → Bool) : AndOrTree → AndOrTree
+| nil                     => nil
+| cons e []               => if p e then cons e [] else nil
+| cons e [c@(cons e' ts)] => if p e' then cons e [AndOrTree.filter p c]
+                             else cons e ts
+| cons e (nil :: ts)         => AndOrTree.filter p <| cons e ts
+| cons e (cons e' ts' :: ts) => if p e' then cons e (cons e' ts' :: AndOrTree.filter p <$> ts)
+                                else AndOrTree.filter p <| cons e ts
+
+/-- Map an expression transformation on an `AndOrTree` -/
+partial def AndOrTree.map (f : Expr → Expr) : AndOrTree → AndOrTree
+| nil       => nil
+| cons e ts => cons (f e) (ts.map <| AndOrTree.map f)
+
+/-- Append at leftmost point, TODO delete -/
+def AndOrTree.append (t : AndOrTree) (e : Expr) : AndOrTree :=
+  match t with
+  | nil               => cons e []
+  | cons e' []        => cons e' [cons e []]
+  | cons e' (t :: ts) => cons e' (t.append e :: ts)
+
 structure State where
-  (conditions : List Expr)
+  (conditions : AndOrTree)
   (pc : Nat)
 
 abbrev M := StateT State MetaM
@@ -33,11 +79,6 @@ def withGetOrMkNewRefs (refs : RefTable) (ns : List ℕ) (types : List Expr) (fv
                               withGetOrMkNewRefs refs' ns ts (fv :: fvs) k
   | [],        []        => k refs fvs
   | _,         _         => panic "types and ref list not the same length!"
-
-def Expr.mkAnds : List Expr → Expr
-| []        => mkConst ``True
-| [e]       => e
-| (e :: es) => mkApp (mkApp (mkConst ``And) e) (mkAnds es)
 
 def mkExistsFVars (fvs : List Expr) (e : Expr) : M Expr :=
   match fvs with
@@ -70,14 +111,14 @@ def withStatementStep (f : SierraFile) (refs : RefTable)
     let fd_types ← mkAppM `List.append #[fd_inputTypes, fd_outputTypes]
     let mut fd_typeList : List Expr := []
     let inputs := s.args
-    let outputs := (s.branches.map BranchInfo.results).join
+    let outputs := (s.branches.map BranchInfo.results).join  -- TODO
     for i in [:(inputs.length+outputs.length)] do
       fd_typeList := fd_typeList ++ [← mkAppM `List.get! #[fd_types, .lit <| .natVal i]]
     withGetOrMkNewRefs refs (inputs ++ outputs).reverse fd_typeList.reverse [] fun refs fvs => do
       let fd_condition ← whnf <| mkAppN fd_condition (fvs.map Expr.fvar).toArray
       -- Only add new condition if it is not trivial
       let conditions' := if ← isDefEq fd_condition (mkConst ``True) then conditions
-                         else conditions ++ [fd_condition]
+                         else conditions.append fd_condition
       let fd : FuncData i' := FuncData_register i'
       let pc' := fd.pcChange pc
       let refs' := fd.refsChange refs (inputs ++ outputs)
@@ -89,14 +130,14 @@ partial def statementLoop (f : SierraFile) (finputs : List (Nat × Identifier))
     (refs : RefTable := HashMap.empty) (gas : ℕ := 25) : M Expr := do
   let ⟨conditions, pc⟩ ← get
   let ⟨i, sinputs, _⟩ := f.statements.get! pc  -- `sinputs` are the returned cells
-  if gas = 0 then return Expr.mkAnds conditions
+  if gas = 0 then return conditions.toExpr
   match i with
   | .name "return" [] =>
     -- Filter out conditions refering to "dangling" FVars (mostly due to `drop()`)
     let conditions := conditions.filter (¬ ·.hasAnyFVar (¬ (refs.toList.map (·.2)).contains ·))
     logInfo "{refs.toList}"
     -- Take the conjunction of all remaining conditions
-    let e := Expr.mkAnds conditions
+    let e := conditions.toExpr
     let (ioRefs, intRefs) := refs.toList.reverse.partition (·.1 ∈ finputs.map (·.1) ++ sinputs)
     -- Existentially close over intermediate references
     let e ← mkExistsFVars (intRefs.map (.fvar ·.2)) e
@@ -111,7 +152,7 @@ def analyzeFile (s : String) : MetaM Format := do
   match parseGrammar s with
   | .ok sf =>
     let ⟨_, pc, inputArgs, _⟩ := sf.declarations.get! 0  -- TODO Don't we need the output types?
-    let e ← StateT.run (statementLoop sf inputArgs) ⟨[], pc⟩
+    let e ← StateT.run (statementLoop sf inputArgs) ⟨.nil, pc⟩
     ppExpr e.1
   | .error str => throwError "Could not parse input file:\n{str}"
 
@@ -121,12 +162,11 @@ def code' :=
 libfunc [0] = felt252_add;
 libfunc [1] = dup<[0]>;
 
-[1]([0]) -> ([0], [1]);
-[0]([0], [1]) -> ([2]);
-[0]([0], [2]) -> ([3]);
-return([3]);
+[0]([0], [1]) -> ([3]);
+[0]([3], [2]) -> ([4]);
+return([4]);
 
-[0]@0([0]: [0]) -> ([0]);"
+[0]@0([0]: [0], [1]: [0], [2]: [0]) -> ([0]);"
 
 #eval analyzeFile code'
 
