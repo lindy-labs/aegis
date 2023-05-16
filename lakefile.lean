9f5437c3206f1fe1b920b8e1e620cc5b114e3320
Jakob von Raumer <jakob@von-raumer.de>
2023-05-16 16:20:20 +0200

    fix straume

--- lakefile.lean
+++ lakefile.lean
@@ -11,14 +11,17 @@ require mathlib from git
 require YatimaStdLib from git
   "https://github.com/lurk-lab/YatimaStdLib.lean" @ "f39dca7a0815ee65e71776d46337f0240037ff6d"
 
+require Straume from git
+  "https://github.com/lurk-lab/straume" @ "053d9feccbface9a0b2c1a72447914376aac74ea"
+
 require Megaparsec from git
   "https://github.com/yatima-inc/Megaparsec.lean" @ "93b28d0ee4be435b6b395d8b6f816fabfc085166"
 
+@[default_target]
 lean_lib SierraLean {
   -- add library configuration options here
 }
 
-@[default_target]
 lean_exe «sierra-lean» {
   root := `Main
 }
