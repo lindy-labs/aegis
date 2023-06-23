506cae9b726d057aa022d5f29641bc21a11193bb
Jakob von Raumer <jakob@von-raumer.de>
2023-06-23 13:51:59 +0200

    implement contract_address_to_felt252

--- SierraLean/FuncData/ContractAddress.lean
+++ SierraLean/FuncData/ContractAddress.lean
@@ -13,6 +13,12 @@ def contract_address_try_from_felt252 : FuncData where
                { outputTypes := [.RangeCheck]
                  condition := fun _ (a : Q(F)) _ => q($(a).val ≤ CONTRACT_ADDRESS_MOD) }]
 
+def contract_address_to_felt252 : FuncData where
+  inputTypes := [.ContractAddress]
+  branches := [{ outputTypes := [.Felt252]
+                 condition := fun (a : Q(ContractAddress)) (ρ : Q(F)) => q($ρ = $(a).cast) }]
+
 def contractAddressLibfuncs : Identifier → Option FuncData
 | .name "contract_address_try_from_felt252" [] .none => contract_address_try_from_felt252
+| .name "contract_address_to_felt252" [] .none => contract_address_to_felt252
 | _ => .none
