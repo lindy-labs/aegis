e325b04888caba4d99601b05da44a1ceba7b936c
Jakob von Raumer <jakob@von-raumer.de>
2023-07-03 17:57:34 +0200

    add missing file

---
+++ SierraLean/FuncData/UInt256.lean
@@ -0,0 +1,17 @@
+import SierraLean.FuncDataUtil
+import Mathlib.Data.ZMod.Basic
+
+open Qq Sierra.SierraType
+
+namespace Sierra.FuncData
+
+def u256_is_zero : FuncData where
+  inputTypes := [.Struct [.U128, .U128]]
+  branches := [{ outputTypes := []
+                 condition := fun (a : Q(UInt128 × UInt128)) => q($a = (0, 0)) },
+               { outputTypes := [.NonZero <| .Struct [.U128, .U128]]
+                 condition := fun (a ρ : Q(UInt128 × UInt128)) => q(($(a).1 ≠ 0 ∨ $(a).2 ≠ 0) ∧ $ρ = $a) }]
+
+def uint256Libfuncs : Identifier → Option FuncData
+| .name "u256_is_zero" [] .none => u256_is_zero
+| _ => .none
