a073cb537bcd22c780747885d43313d0aebd677f
Jakob von Raumer <jakob@von-raumer.de>
2023-07-03 17:06:13 +0200

    remove dbg trace

--- SierraLean/Commands.lean
+++ SierraLean/Commands.lean
@@ -76,7 +76,6 @@ initialize loadedSierraFile : SimplePersistentEnvExtension SierraFile (Option Si
       for sfs in sfss do
         for sf in sfs do
           if sf.declarations.length > 0 then
-            dbg_trace "overwrite sierra file with one that contains decls {sf.declarations.head!.1}, ..."
             sf? := sf
       sf?
   }
