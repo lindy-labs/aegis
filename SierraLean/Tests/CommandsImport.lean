28090cdeffffab4fe4c69ad3652a6656c4700528
Jakob von Raumer <jakob@von-raumer.de>
2023-07-10 18:12:08 +0200

    fix tests

--- SierraLean/Tests/CommandsImport.lean
+++ SierraLean/Tests/CommandsImport.lean
@@ -3,4 +3,5 @@ import SierraLean.Tests.Commands
 sierra_spec "test::bar" := fun _ a b ρ => (ρ : Bool) = xor a b
 
 sierra_sound "test::bar" := fun _ a b ρ => by
+  simp only [Bool.toSierraBool, «spec_test::bar»]
   aesop
