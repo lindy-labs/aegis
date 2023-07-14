ad69c5ffcb5a2dcf001808d20d0e70f3afc95b78
Jakob von Raumer <jakob@von-raumer.de>
2023-07-14 16:14:47 +0200

    implement u256_safe_divmod

--- SierraLean/FuncData/UInt256.lean
+++ SierraLean/FuncData/UInt256.lean
@@ -3,9 +3,7 @@ import Mathlib.Data.ZMod.Basic
 
 open Qq Sierra.SierraType
 
-namespace Sierra
-
-namespace FuncData
+namespace Sierra.FuncData
 
 def u256_is_zero : FuncData where
   inputTypes := [.Struct [.U128, .U128]]
@@ -14,6 +12,18 @@ def u256_is_zero : FuncData where
                { outputTypes := [.NonZero <| .Struct [.U128, .U128]]
                  condition := fun (a ρ : Q(UInt128 × UInt128)) => q(($(a).1 ≠ 0 ∨ $(a).2 ≠ 0) ∧ $ρ = $a) }]
 
+def u256_safe_divmod : FuncData where
+  inputTypes := [.RangeCheck, .Struct [.U128, .U128], .Struct [.U128, .U128]]
+  branches := [{ outputTypes := [.RangeCheck, .Struct [.U128, .U128], .Struct [.U128, .U128]]
+                 condition := fun _ (a b : Q(UInt128 × UInt128))
+                   _ (div mod : Q(UInt128 × UInt128)) =>
+                   -- TODO maybe replace by a direct characterization in the future
+                   q(U128_MOD * $(div).2.val + $(div).1.val =
+                       (U128_MOD * $(a).2.val + $(a).1.val) / (U128_MOD * $(b).2.val + $(b).1.val)
+                     ∧ U128_MOD * $(mod).2.val + $(mod).1.val =
+                       (U128_MOD * $(a).2.val + $(a).1.val) % (U128_MOD * $(b).2.val + $(b).1.val)) }]
+
 def uint256Libfuncs : Identifier → Option FuncData
 | .name "u256_is_zero" [] .none => u256_is_zero
+| .name "u256_safe_divmod" [] .none => u256_safe_divmod
 | _ => .none
