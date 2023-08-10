1c42d6a82224ea0cd4a8ebed31ed37f22ba07b6a
Jakob von Raumer <jakob@von-raumer.de>
2023-08-10 15:28:58 +0200

    fix branch order in contract_address_try_from_felt252 implementation

--- SierraLean/FuncData/ContractAddress.lean
+++ SierraLean/FuncData/ContractAddress.lean
@@ -9,9 +9,9 @@ def contract_address_try_from_felt252 : FuncData where
   inputTypes := [.RangeCheck, .Felt252]
   branches := [{ outputTypes := [.RangeCheck, .ContractAddress]
                  condition := fun _ (a : Q(F)) _ (ρ : Q(ContractAddress)) =>
-                   q(CONTRACT_ADDRESS_MOD < $(a).val ∧ $ρ = $(a).cast) },
+                   q($(a).val < CONTRACT_ADDRESS_MOD ∧ $ρ = $(a).cast) },
                { outputTypes := [.RangeCheck]
-                 condition := fun _ (a : Q(F)) _ => q($(a).val ≤ CONTRACT_ADDRESS_MOD) }]
+                 condition := fun _ (a : Q(F)) _ => q(CONTRACT_ADDRESS_MOD ≤ $(a).val) }]
 
 def contract_address_to_felt252 : FuncData where
   inputTypes := [.ContractAddress]
