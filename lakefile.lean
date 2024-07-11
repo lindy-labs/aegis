b5e24d6d38e9c4bcffc949407b14751cbe916a3c
Jakob von Raumer <jakob@von-raumer.de>
2024-07-11 14:32:24 +0200

    update mathlib and lean again

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ package «aegis» {
 }
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4" @ "v4.9.1"
+  "https://github.com/leanprover-community/mathlib4" @ "v4.10.0-rc2"
 
 @[default_target]
 lean_lib Aegis {
